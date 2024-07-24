9c60b8d50c2daec059914c6c1f69efd5638ad889
shubham43MP <shubhamdixit19950321@gmail.com>
2024-07-24 20:47:21 +0530

    feat: removed material ui in favour of material tailwind lateron

--- src/components/PersonasMenu/index.ts
+++
@@ -1,1 +0,0 @@
-export { PersonasMenu } from './PersonasMenu';
--- src/containers/HomePage/HomePage.tsx
+++ src/containers/HomePage/HomePage.tsx
@@ -1,10 +1,3 @@
-import { PersonasMenu } from '../../components/PersonasMenu';
-import './HomePage.scss';
-
 export const HomePage = () => {
-  return (
-    <div className="flex justify-center mt-8">
-      <PersonasMenu />
-    </div>
-  );
+  return <div className="flex justify-center mt-8">test</div>;
 };
