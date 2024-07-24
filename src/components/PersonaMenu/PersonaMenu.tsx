996f8fcf7e5015bc137286684a3c1fa1f72b6ddc
shubham43MP <shubhamdixit19950321@gmail.com>
2024-07-24 23:56:13 +0530

    converted to buttons

--- src/components/PersonaMenu/PersonaMenu.tsx
+++ src/components/PersonaMenu/PersonaMenu.tsx
@@ -12,14 +12,14 @@ const Item = () => {
 export const PersonaMenu = () => {
   return (
     <div className="p-4 rounded-lg w-96 max-w-sm">
-      <div className="flex px-2 py-3">
+      <button className="flex px-2 py-3">
         <IconComp imgUrl="/icons/favorites.svg" />
         <p className="ml-2 text-lg font-medium">Favourites</p>
-      </div>
-      <div className="flex px-2 py-3">
+      </button>
+      <button className="flex px-2 py-3">
         <IconComp imgUrl="/icons/twopeople.svg" />
         <p className="ml-2 text-lg font-medium">Shared with me</p>
-      </div>
+      </button>
       <hr className="border-solid border-black-700"></hr>
       <div className=""></div>
       <AccordionCustom
@@ -46,48 +46,48 @@ export const PersonaMenu = () => {
               }
               BodyContent={
                 <div className="">
-                  <div className="flex gap-3 px-2 py-2 pl-8">
+                  <button className="flex gap-3 px-2 py-2 pl-8">
                     <IconComp
                       imgUrl="/icons/journeymaps.svg"
                       customClass="h-5 w-5"
                     />
                     <p className="text-base font-medium">Journey Maps</p>
-                  </div>
-                  <div className="flex gap-3 px-2 py-2 pl-8">
+                  </button>
+                  <button className="flex gap-3 px-2 py-2 pl-8">
                     <IconComp
                       imgUrl="/icons/onepeople.svg"
                       customClass="h-5 w-5"
                     />
                     <p className="text-base font-medium">Personas</p>
-                  </div>
-                  <div className="flex gap-3 px-2 py-2 pl-8">
+                  </button>
+                  <button className="flex gap-3 px-2 py-2 pl-8">
                     <IconComp
                       imgUrl="/icons/templates.svg"
                       customClass="h-5 w-5"
                     />
                     <p className="text-base font-medium">Templates</p>
-                  </div>
-                  <div className="flex gap-3 px-2 py-2 pl-8">
+                  </button>
+                  <button className="flex gap-3 px-2 py-2 pl-8">
                     <IconComp
                       imgUrl="/icons/portfolio.svg"
                       customClass="h-5 w-5"
                     />
                     <p className="text-base font-medium">Portfolio</p>
-                  </div>
-                  <div className="flex gap-3 px-2 py-2 pl-8">
+                  </button>
+                  <button className="flex gap-3 px-2 py-2 pl-8">
                     <IconComp
                       imgUrl="/icons/archives.svg"
                       customClass="h-5 w-5"
                     />
                     <p className="text-base font-medium">Archives</p>
-                  </div>
-                  <div className="flex gap-3 px-2 py-2 pl-8">
+                  </button>
+                  <button className="flex gap-3 px-2 py-2 pl-8">
                     <IconComp
                       imgUrl="/icons/settings.svg"
                       customClass="h-5 w-5"
                     />
                     <p className="text-base font-medium">Settings</p>
-                  </div>
+                  </button>
                 </div>
               }
             />
