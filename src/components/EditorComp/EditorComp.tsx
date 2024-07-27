320a3688fb7d7d228c4595ed8e413b3851b9e6c5
shubham43MP <shubhamdixit19950321@gmail.com>
2024-07-28 00:07:06 +0530

    added minor css customisations

--- src/components/EditorComp/EditorComp.tsx
+++ src/components/EditorComp/EditorComp.tsx
@@ -3,6 +3,7 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { EditIcon } from '../../assets/Icons/edit';
 import { PortfolioIcon } from '../../assets/Icons';
+import './editor.scss';
 
 export type EditorCompProps = {
   preview?: boolean;
@@ -27,9 +28,9 @@ export const EditorComp = ({ preview = false }: EditorCompProps) => {
           )}
         </button>
         {prevw ? (
-          <div className="ql-snow">
+          <div className="ql-snow ">
             <div
-              className="h-full p-2 ql-editor"
+              className="h-full p-2 ql-editor c-editor text-lightgray rounded-lg"
               dangerouslySetInnerHTML={{ __html: richText }}
             />
           </div>
