568d6c05dd7c17197ea921f9fa79016da304b76d
shubham43MP <shubhamdixit19950321@gmail.com>
2024-07-31 23:50:31 +0530

    ts formatting update

--- src/components/compounds/EditableRichTextComp/types.ts
+++ src/components/compounds/EditableRichTextComp/types.ts
@@ -1,13 +1,13 @@
 import { LimitedNumericValueFlag } from "@/utils/types";
 
 export type EditableRichTextCompProps = {
-    preview?: boolean;
-    richText: string;
-    richTextChangeHandler: (
-      value: string,
-      identifier: string,
-      flag: LimitedNumericValueFlag
-    ) => void;
-    identifier: string;
-    flag: LimitedNumericValueFlag;
-  };
+  preview?: boolean;
+  richText: string;
+  richTextChangeHandler: (
+    value: string,
+    identifier: string,
+    flag: LimitedNumericValueFlag
+  ) => void;
+  identifier: string;
+  flag: LimitedNumericValueFlag;
+};
