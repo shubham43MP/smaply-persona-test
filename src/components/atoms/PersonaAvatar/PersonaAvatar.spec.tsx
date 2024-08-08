bcaa833c277792f7c8b67bdb42348381f98131c5
shubham43MP <shubhamdixit19950321@gmail.com>
2024-08-08 17:18:35 +0530

    written 100% code coverage for persona avatar component

--- src/components/atoms/PersonaAvatar/PersonaAvatar.spec.tsx
+++ src/components/atoms/PersonaAvatar/PersonaAvatar.spec.tsx
@@ -1,12 +1,36 @@
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom'; // for the "toBeInTheDocument" matcher
 import { PersonaAvatar } from './PersonaAvatar';
 
-describe('Persona Avatar', () => {
-  test('renders Persona Avatar', async () => {
-    render(
-      <PersonaAvatar>
-        <>test heading</>
-      </PersonaAvatar>
+describe('PersonaAvatar', () => {
+  it('renders the children correctly', () => {
+    const { getByText } = render(<PersonaAvatar>Test Child</PersonaAvatar>);
+    expect(getByText('Test Child')).toBeInTheDocument();
+  });
+
+  it('applies custom class correctly', () => {
+    const customClass = 'custom-class';
+    const { container } = render(
+      <PersonaAvatar customClass={customClass}>Test Child</PersonaAvatar>
     );
+    expect(container.firstChild).toHaveClass(customClass);
+  });
+
+  it('applies default classes correctly', () => {
+    const { container } = render(<PersonaAvatar>Test Child</PersonaAvatar>);
+    expect(container.firstChild).toHaveClass(
+      'p-3 flex items-center justify-center rounded-xl'
+    );
+  });
+
+  it('applies both default and custom classes correctly', () => {
+    const customClass = 'custom-class';
+    const { container } = render(
+      <PersonaAvatar customClass={customClass}>Test Child</PersonaAvatar>
+    );
+    expect(container.firstChild).toHaveClass(
+      'p-3 flex items-center justify-center rounded-xl'
+    );
+    expect(container.firstChild).toHaveClass(customClass);
   });
 });
