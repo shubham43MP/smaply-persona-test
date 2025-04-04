3b012a559304d934596167cf704b03bced8ac6c0
Tushar Sharma <tsharma.hotbitinfosoft.com>
2025-04-04 13:20:15 +0530

    Navbar testcases passed

--- src/components/compounds/Navbar/Navbar.spec.tsx
+++ src/components/compounds/Navbar/Navbar.spec.tsx
@@ -36,14 +36,39 @@ describe('Navbar Component', () => {
     expect(hrElement).toHaveClass('mx-6 border-solid border-slate-200');
   });
 
+  // it('renders the layout correctly', () => {
+  //   render(<Navbar />);
+  //   const headerElement = screen.getByRole('banner');
+  //   expect(headerElement).toHaveClass('mx-8 my-6');
+
+  //   const divElements = screen.getAllByRole('presentation');
+  //   expect(divElements[0]).toHaveClass('flex justify-between');
+  //   expect(divElements[1]).toHaveClass('flex gap-3');
+  // });
+
   it('renders the layout correctly', () => {
-    render(<Navbar />);
+    const { container } = render(<Navbar />);
+
+    // Ensure the header has the correct classes
     const headerElement = screen.getByRole('banner');
     expect(headerElement).toHaveClass('mx-8 my-6');
-    
-    const divElements = screen.getAllByRole('presentation');
-    expect(divElements[0]).toHaveClass('flex justify-between');
-    expect(divElements[1]).toHaveClass('flex gap-3');
+
+    // Ensure the logo is rendered correctly
+    const logo = screen.getByAltText('Smaply Logo');
+    expect(logo).toBeInTheDocument();
+
+    // Ensure the user's name is rendered
+    const userName = screen.getByText('John Smith');
+    expect(userName).toBeInTheDocument();
+
+    // Ensure the avatar is rendered
+    const avatar = screen.getByRole('img');
+    expect(avatar).toBeInTheDocument();
+
+    // Get divs by class name using querySelectorAll
+    const divElements = container.querySelectorAll('.flex');
+    expect(divElements[0]).toHaveClass('justify-between');
+    expect(divElements[1]).toHaveClass('gap-3');
   });
 
   it('matches the snapshot', () => {
